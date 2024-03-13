2115266fc7693e8bdc418dc81d736f79d23d7a74
Mohd Adil <adil88.mohd@gmail.com>
2024-03-14 03:28:19 +0530

    modify Sign In page

---
+++ src/component/OAuth.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const OAuth = () => {
+  return (
+    <>
+      <button className="bg-red-700 border-2 border-red-800 p-3 w-full rounded-lg text-white uppercase">
+        continue with google
+      </button>
+    </>
+  );
+};
+
+export default OAuth;
