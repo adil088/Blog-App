babb92ed5183da0554d4b247d030420c29931c90
Mohd Adil <adil88.mohd@gmail.com>
2024-03-14 02:53:42 +0530

    Complete Sign in UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import './App.css'
+import Header from './component/Header'
+import { BrowserRouter, Route } from 'react-router-dom'
+import { Routes } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import SignIn from './pages/SignIn.jsx'
+import SignUp from './pages/SignUp.jsx'
 
 function App() {
 
   return (
     <>
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+     <Header/>
+     <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/sign-in" element={<SignIn/>} />
+      <Route path="/sign-up" element={<SignUp/>} />
+     </Routes>
+     </BrowserRouter>
     </>
   )
 }
---
+++ src/component/Header.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { FaSearch } from "react-icons/fa";
+import {Link} from 'react-router-dom'
+
+function Header() {
+  return (
+    <header className="bg-blue-200 shadow-md">
+        <div className="flex justify-between items-center mx-auto max-w-6xl p-3">
+        <Link to='/'>
+            <h1 className="font-bold text-slate-700">Blog App</h1>
+            </Link>
+            <form className="bg-blue-100 border-2 border-blue-300 p-3 rounded-lg flex items-center">
+                <input type="text" name="" placeholder="Search..." className="bg-transparent focus:outline-none w-24 sm:w-64" />
+                <button>
+                    <FaSearch className="text-slate-600"/>
+                </button>
+            </form>
+            <ul className="flex gap-2 sm:gap-4">
+                <Link to='/'>
+                <li className="hidden sm:inline hover:underline cursor-pointer text-slate-700">Home</li>
+                </Link>
+                <Link to='/about'>
+                <li className="hidden sm:inline hover:underline cursor-pointer text-slate-700">About</li>
+                </Link>
+                <Link to='/sign-in'>
+                <li className="hover:underline cursor-pointer text-slate-700">Sign in</li>
+                </Link>
+            </ul>
+        </div>
+    </header>
+  );
+}
+
+export default Header;
